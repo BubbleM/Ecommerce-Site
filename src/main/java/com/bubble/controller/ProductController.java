7126306934974dce5407a8ed7245b1902d5e69f1
BubbleM <1870408473@qq.com>
2017-06-13 20:15:12 +0800

    完成商品管理模块　实现客户端的增删改查

--- src/main/java/com/bubble/controller/ProductController.java
+++ src/main/java/com/bubble/controller/ProductController.java
@@ -1,8 +1,140 @@
 package com.bubble.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.bubble.dao.ProductDao;
+import com.bubble.entity.ProductEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by bubble on 17-6-8.
  */
+@Controller
+@RequestMapping("/product")
 public class ProductController {
 
+    ProductDao dao = new ProductDao();
+
+    @RequestMapping("/getProducts.do")
+    @ResponseBody
+    public void getProducts(HttpServletResponse response){
+        List<ProductEntity> products = dao.getProducts();
+        System.out.println("get product info!");
+        response.setCharacterEncoding("utf-8");
+        Writer writer;
+        try{
+           writer = response.getWriter();
+           String jsonString = JSON.toJSONString(products);
+//           System.out.println(jsonString);
+           writer.append(JSON.toJSONString(products));
+           writer.flush();
+           writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @RequestMapping("/add.do")
+    public void productAdd(String name,String type,String description,HttpServletResponse response){
+        ProductEntity product = new ProductEntity();
+        product.setName(name);
+        product.setType(type);
+        product.setDescription(description);
+        int r = dao.insertProduct(product);
+        response.setCharacterEncoding("utf-8");
+        Writer writer = null;
+        try {
+            writer = response.getWriter();
+            if(r>0){
+                writer.append("OK");
+            }else{
+                writer.append("NO");
+            }
+            writer.flush();
+        }catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally{
+            if(writer != null){
+                try {
+                    writer.close();
+                    writer = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @RequestMapping("/delete.do")
+    public void productDelete(Integer id,HttpServletResponse response){
+        int r = dao.deleteProduct(id);
+        response.setCharacterEncoding("utf-8");
+        Writer writer = null;
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            writer = response.getWriter();
+            if(r>0){
+                System.out.println("delete success!");
+                map.put("success", true);
+            }else{
+                map.put("success", false);
+                System.out.println("delete fail!");
+            }
+            writer.append(JSON.toJSONString(map));
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            if(writer != null){
+                try {
+                    writer.close();
+                    writer = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @RequestMapping("/updateProduct.do")
+    public void updateUser(Integer id,String name,String type,String description,HttpServletResponse response){
+        System.out.println("*****************update Product!************");
+        System.out.println("id="+id+" name="+name+" type="+type+" description"+description);
+        ProductEntity product = dao.findProductById(id);
+        product.setName(name);
+        product.setType(type);
+        product.setDescription(description);
+        int r =  dao.saveOrUpdateProduct(product);
+        response.setCharacterEncoding("utf-8");
+        Writer writer = null;
+        try {
+            writer = response.getWriter();
+            if(r>0){
+                writer.append("OK");
+            }else{
+                writer.append("NO");
+            }
+            writer.flush();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally{
+            if(writer != null){
+                try {
+                    writer.close();
+                    writer = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/bubble/dao/ProductDao.java
+++ src/main/java/com/bubble/dao/ProductDao.java
@@ -14,17 +14,21 @@ public class ProductDao {
     /*
     * 添加商品
     * */
-    public void insertProduct(ProductEntity product){
+    public int insertProduct(ProductEntity product){
         Session session = null; //声明Session对象
+        int result;
         try {
             session = HibernateUtil.getSession(); //获取Session
             session.beginTransaction();//开启事务
             session.save(product);  //保存用户信息
             session.getTransaction().commit(); //提交事务
+            result = 1;
         }catch (Exception e){
+            result = 0;
             e.printStackTrace();
             session.getTransaction().rollback();
         }
+        return result;
     }
 
     /*
@@ -77,12 +81,90 @@ public class ProductDao {
         return false;
     }
 
+    /*
+   * 查找数据库中所有product信息
+   * */
+    public List<ProductEntity> getProducts(){
+        Session session = null;
+        List<ProductEntity> list = null;
+        try{
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            String hql = "from ProductEntity u";
+            Query query = session.createQuery(hql);
+            list = query.list();
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            session.getTransaction();
+        }
+        return list;
+    }
+
+    /*
+   * 管理员根据id删除相应商品
+   */
+    public int deleteProduct(Integer id){
+        int result;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            ProductEntity product = session.load(ProductEntity.class,new Integer(id));
+            session.delete(product);
+            session.getTransaction().commit();
+            System.out.println("删除成功"+id);
+            result = 1;
+        }catch (Exception e){
+            result = 0;
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        return result;
+    }
+
+    /*
+   * 保存或更改product对象
+   * */
+    public int saveOrUpdateProduct(ProductEntity product){
+        Session session = null;
+        int result;
+        try{
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            session.saveOrUpdate(product);
+            session.getTransaction().commit();
+            result = 1;
+        }catch (Exception e){
+            result = 0;
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        return result;
+    }
+    public ProductEntity findProductById(Integer id){
+        Session session = null;
+        ProductEntity product = null;
+        try{
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            product = (ProductEntity) session.get(ProductEntity.class,id);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        return product;
+    }
+
+
     public static void main(String[] args){
         ProductDao dao = new ProductDao();
+//        System.out.println(dao.getProducts());
 //        ProductEntity product = new ProductEntity();
-//        product.setName("Tphone");
-//        product.setType("elep");
-//        product.setDescription("this is apple phone");
+//        product.setName("LenovoR720");
+//        product.setType("electronic_product");
+//        product.setDescription("computer");
 //        dao.insertProduct(product);
 //        System.out.println(dao.findProductByName("Tphone"));
 //        dao.findProductByType("clothing");
